e94e7a844202f1301ba81534c1401fe0f2d1445e
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-07 00:54:33 +0530

    Register document store properties

--- services/user-chef-service/src/main/java/in/craves/userchef/UserChefServiceApplication.java
+++ services/user-chef-service/src/main/java/in/craves/userchef/UserChefServiceApplication.java
@@ -1,13 +1,13 @@
 package in.craves.userchef;
 
-import in.craves.userchef.config.BlobStorageProperties;
 import in.craves.userchef.config.CravesJwtProperties;
+import in.craves.userchef.config.DocumentStoreProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({CravesJwtProperties.class, BlobStorageProperties.class})
+@EnableConfigurationProperties({CravesJwtProperties.class, DocumentStoreProperties.class})
 public class UserChefServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserChefServiceApplication.class, args);

d2a5865f0acf3b0776d663489545403c77ab9b6c
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-07 03:46:53 +0530

    Register User Chef Auth internal client properties

--- services/user-chef-service/src/main/java/in/craves/userchef/UserChefServiceApplication.java
+++ services/user-chef-service/src/main/java/in/craves/userchef/UserChefServiceApplication.java
@@ -1,5 +1,6 @@
 package in.craves.userchef;
 
+import in.craves.userchef.config.AuthInternalClientProperties;
 import in.craves.userchef.config.CravesJwtProperties;
 import in.craves.userchef.config.DocumentStoreProperties;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({CravesJwtProperties.class, DocumentStoreProperties.class})
+@EnableConfigurationProperties({CravesJwtProperties.class, DocumentStoreProperties.class, AuthInternalClientProperties.class})
 public class UserChefServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserChefServiceApplication.class, args);

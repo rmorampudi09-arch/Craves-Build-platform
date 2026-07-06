521f4fe3822b93999c77a4882ea5d96963b92475
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-06 20:10:40 +0530

    Add auth service application class

---
+++ services/auth-service/src/main/java/in/craves/auth/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package in.craves.auth;
+
+import in.craves.auth.config.FirebaseProperties;
+import in.craves.auth.config.JwtProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({FirebaseProperties.class, JwtProperties.class})
+public class AuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}

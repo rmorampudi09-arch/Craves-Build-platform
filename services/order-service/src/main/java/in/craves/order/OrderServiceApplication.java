87eb4dbf290135469224a088e3f2a3dbb676d7a2
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-08 02:23:17 +0530

    Enable scheduling for notification outbox dispatcher

--- services/order-service/src/main/java/in/craves/order/OrderServiceApplication.java
+++ services/order-service/src/main/java/in/craves/order/OrderServiceApplication.java
@@ -3,12 +3,20 @@ package in.craves.order;
 import in.craves.order.config.CatalogClientProperties;
 import in.craves.order.config.CravesJwtProperties;
 import in.craves.order.config.NotificationClientProperties;
+import in.craves.order.config.NotificationOutboxDispatcherProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties({CravesJwtProperties.class, CatalogClientProperties.class, NotificationClientProperties.class})
+@EnableScheduling
+@EnableConfigurationProperties({
+    CravesJwtProperties.class,
+    CatalogClientProperties.class,
+    NotificationClientProperties.class,
+    NotificationOutboxDispatcherProperties.class
+})
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);

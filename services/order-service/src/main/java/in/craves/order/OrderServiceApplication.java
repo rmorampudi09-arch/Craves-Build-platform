9bdbe257895791bf3588c821173b95d8a28b9686
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-08 00:58:31 +0530

    Register Order notification properties

--- services/order-service/src/main/java/in/craves/order/OrderServiceApplication.java
+++ services/order-service/src/main/java/in/craves/order/OrderServiceApplication.java
@@ -2,12 +2,13 @@ package in.craves.order;
 
 import in.craves.order.config.CatalogClientProperties;
 import in.craves.order.config.CravesJwtProperties;
+import in.craves.order.config.NotificationClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({CravesJwtProperties.class, CatalogClientProperties.class})
+@EnableConfigurationProperties({CravesJwtProperties.class, CatalogClientProperties.class, NotificationClientProperties.class})
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);

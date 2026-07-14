80dcd549b5ae0ce78d14b00967533d11a40822f0
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-14 18:37:24 +0530

    Configure Azure Service Bus delivery clients

---
+++ services/integration-service/src/main/java/in/craves/integration/delivery/command/DeliveryServiceBusConfiguration.java
@@ -0,0 +1,61 @@
+package in.craves.integration.delivery.command;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+@Configuration
+@ConditionalOnProperty(prefix = "craves.delivery-command", name = "enabled", havingValue = "true")
+public class DeliveryServiceBusConfiguration {
+
+    @Bean
+    DeliveryServiceBusClientFactory deliveryServiceBusClientFactory(DeliveryCommandProperties properties) {
+        return new DeliveryServiceBusClientFactory(properties);
+    }
+
+    @Bean(destroyMethod = "close")
+    @Qualifier("deliveryCommandSender")
+    ServiceBusSenderClient deliveryCommandSender(DeliveryServiceBusClientFactory factory,
+                                                  DeliveryCommandProperties properties) {
+        return factory.newBuilder()
+            .sender()
+            .queueName(properties.getQueueName())
+            .buildClient();
+    }
+
+    @Bean(destroyMethod = "close")
+    @Qualifier("deliveryDomainEventSender")
+    ServiceBusSenderClient deliveryDomainEventSender(DeliveryServiceBusClientFactory factory,
+                                                      DeliveryCommandProperties properties) {
+        return factory.newBuilder()
+            .sender()
+            .topicName(properties.getTopicName())
+            .buildClient();
+    }
+
+    public static final class DeliveryServiceBusClientFactory {
+        private final DeliveryCommandProperties properties;
+        private final TokenCredential credential;
+
+        DeliveryServiceBusClientFactory(DeliveryCommandProperties properties) {
+            this.properties = properties;
+            this.credential = StringUtils.hasText(properties.getConnectionString())
+                ? null
+                : new DefaultAzureCredentialBuilder().build();
+        }
+
+        ServiceBusClientBuilder newBuilder() {
+            ServiceBusClientBuilder builder = new ServiceBusClientBuilder();
+            if (StringUtils.hasText(properties.getConnectionString())) {
+                return builder.connectionString(properties.getConnectionString());
+            }
+            return builder.credential(properties.getFullyQualifiedNamespace(), credential);
+        }
+    }
+}

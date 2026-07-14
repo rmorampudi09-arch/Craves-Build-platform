6e4bbe92d4cbf5bb3c231111dc7e5ee0f0db5023
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-14 14:52:12 +0530

    Enable delivery intelligence maintenance scheduling

---
+++ services/integration-service/src/main/java/in/craves/integration/delivery/DeliverySchedulingConfiguration.java
@@ -0,0 +1,9 @@
+package in.craves.integration.delivery;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class DeliverySchedulingConfiguration {
+}

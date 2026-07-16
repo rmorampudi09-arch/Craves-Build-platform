9fb5f05e971115449b7071fa049f4dc0362a9dd2
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-16 19:15:39 +0530

    Enable scheduled integration workers

--- services/integration-service/src/main/java/in/craves/integration/IntegrationServiceApplication.java
+++ services/integration-service/src/main/java/in/craves/integration/IntegrationServiceApplication.java
@@ -2,8 +2,10 @@ package in.craves.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IntegrationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(IntegrationServiceApplication.class, args);

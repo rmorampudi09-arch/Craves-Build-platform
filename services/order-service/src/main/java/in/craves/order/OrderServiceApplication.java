b96bc64f521b0a9517f33cb1fefa53554e0529d1
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-07-16 14:04:33 +0530

    Keep direct environment binding local to address client

--- services/order-service/src/main/java/in/craves/order/OrderServiceApplication.java
+++ services/order-service/src/main/java/in/craves/order/OrderServiceApplication.java
@@ -4,7 +4,6 @@ import in.craves.order.config.CatalogClientProperties;
 import in.craves.order.config.CravesJwtProperties;
 import in.craves.order.config.NotificationClientProperties;
 import in.craves.order.config.NotificationOutboxDispatcherProperties;
-import in.craves.order.config.UserChefInternalClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -15,7 +14,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties({
     CravesJwtProperties.class,
     CatalogClientProperties.class,
-    UserChefInternalClientProperties.class,
     NotificationClientProperties.class,
     NotificationOutboxDispatcherProperties.class
 })

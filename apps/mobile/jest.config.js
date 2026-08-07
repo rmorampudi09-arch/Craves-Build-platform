db7ee915f89040e48564b2a9cba7ad69e98fbd54
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-08-07 16:05:50 +0530

    Transform Phase 1 React Native ESM dependencies in Jest

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    'node_modules/(?!((@)?react-native|@react-native-community|@react-navigation|react-redux|@reduxjs/toolkit|@tanstack|expo|@expo|expo-modules-core|@react-native-firebase)/)',
+  ],
 };

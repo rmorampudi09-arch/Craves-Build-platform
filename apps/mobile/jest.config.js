25acf2ad8c6978227d980bd8e08b42cd136f2e23
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-08-07 16:13:56 +0530

    Transform Redux transitive ESM dependencies in Phase 1 Jest

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: '@react-native/jest-preset',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native.*|@react-native.*|@react-navigation.*|react-redux|@reduxjs/toolkit.*|@tanstack.*|expo.*|@expo.*)/)',
+    'node_modules/(?!((jest-)?react-native.*|@react-native.*|@react-navigation.*|react-redux|@reduxjs/toolkit.*|redux|redux-thunk|reselect|immer|@tanstack.*|expo.*|@expo.*)/)',
   ],
 };

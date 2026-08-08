a3978209724a7a4bc66a5651d8210124b3fd3b44
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-08-08 05:35:33 +0530

    feat(mobile): register shared customer shell state

--- apps/mobile/src/app/store/store.ts
+++ apps/mobile/src/app/store/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {authReducer} from '../../features/auth/state/authSlice';
+import {customerShellReducer} from '../../features/customerShell/state/customerShellSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    customerShell: customerShellReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({serializableCheck: true, immutableCheck: __DEV__}),

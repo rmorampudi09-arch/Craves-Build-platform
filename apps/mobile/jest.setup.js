cf3e2ff69d19d800c1ac01e50b50bb333aecbd70
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-08-09 06:01:27 +0530

    test(mobile): mock AsyncStorage for P74 settings

--- apps/mobile/jest.setup.js
+++ apps/mobile/jest.setup.js
@@ -8,6 +8,10 @@ jest.mock('react-native-config', () => ({
   },
 }));
 
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
 jest.mock('expo-secure-store', () => ({
   getItemAsync: jest.fn(async () => null),
   setItemAsync: jest.fn(async () => undefined),

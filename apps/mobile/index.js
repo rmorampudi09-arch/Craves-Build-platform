f997c728d25b514933e55ab93848cba2e11c70b9
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-08-10 14:33:15 +0530

    P120 install JS crash observation hook

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,5 +1,8 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+import {installGlobalErrorObservation} from './src/core/observability/globalErrorObservation';
 import App from './App';
 import {name as appName} from './app.json';
+
+installGlobalErrorObservation();
 AppRegistry.registerComponent(appName, () => App);

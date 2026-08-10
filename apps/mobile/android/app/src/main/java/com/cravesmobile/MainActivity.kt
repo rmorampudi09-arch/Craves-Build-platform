e9e51c67bfea0a86865e7009d36b8a64431faf58
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-08-10 22:59:07 +0530

    fix(mobile): wrap Android activity for Expo modules

--- apps/mobile/android/app/src/main/java/com/cravesmobile/MainActivity.kt
+++ apps/mobile/android/app/src/main/java/com/cravesmobile/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.cravesapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -14,9 +20,12 @@ class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "CravesMobile"
 
   /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * Wrap the React Native delegate so Expo modules receive the Android lifecycle hooks they need.
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    ReactActivityDelegateWrapper(
+      this,
+      BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+      object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {},
+    )
 }

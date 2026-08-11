409f86e479568219212ca65e685c28e6e45e636a
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-08-11 20:41:56 +0530

    Fix occurrence generator Spring constructor wiring

--- services/subscription-service/src/main/java/in/craves/subscription/occurrence/OccurrenceGeneratorService.java
+++ services/subscription-service/src/main/java/in/craves/subscription/occurrence/OccurrenceGeneratorService.java
@@ -18,6 +18,7 @@ import java.util.Map;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,6 +29,7 @@ public class OccurrenceGeneratorService {
     private final OccurrenceRepository repository;
     private final Clock clock;
 
+    @Autowired
     public OccurrenceGeneratorService(
         OccurrenceGeneratorProperties properties,
         OccurrenceRepository repository

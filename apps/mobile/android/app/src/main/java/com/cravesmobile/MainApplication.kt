6e553d70a5ddf9984db8be569fb550e5f53a7d63
rmorampudi09-arch <rmorampudi09@gmail.com>
2026-08-14 00:29:19 +0530

    Register customer foreground location module

--- apps/mobile/android/app/src/main/java/com/cravesmobile/MainApplication.kt
+++ apps/mobile/android/app/src/main/java/com/cravesmobile/MainApplication.kt
@@ -16,8 +16,7 @@ class MainApplication : Application(), ReactApplication {
       context = applicationContext,
       packageList =
         PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
+          add(CravesCurrentLocationPackage())
         },
     )
   }
@@ -30,6 +29,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onConfigurationChanged(newConfig: Configuration) {
     super.onConfigurationChanged(newConfig)
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+    ApplicationLifecycleDispatcher.onApplicationConfigurationChanged(this, newConfig)
   }
 }

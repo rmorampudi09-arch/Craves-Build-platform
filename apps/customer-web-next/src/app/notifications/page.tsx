4fd4a99a98c343ad08cd2055002baddc88939ad9
gopicraves <gopi.k@craves.in>
2026-09-21 22:16:39 +0530

    Use global customer navigation on notifications

--- apps/customer-web-next/src/app/notifications/page.tsx
+++ apps/customer-web-next/src/app/notifications/page.tsx
@@ -1,11 +1,3 @@
 "use client";
-import { BottomNav } from "@/components/layout/BottomNav";
 import NotificationsPage from "@/screens/Notifications/Notifications";
-export default function NotificationsRoutePage() {
-  return (
-    <div className="pb-20 md:pb-0">
-      <NotificationsPage />
-      <BottomNav />
-    </div>
-  );
-}
+export default NotificationsPage;

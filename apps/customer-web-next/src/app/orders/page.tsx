209307fc51687c8e8d2052264362ef7b6871f54f
gopicraves <gopi.k@craves.in>
2026-09-21 22:16:31 +0530

    Use global customer navigation on orders

--- apps/customer-web-next/src/app/orders/page.tsx
+++ apps/customer-web-next/src/app/orders/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import { BottomNav } from "@/components/layout/BottomNav";
 import { ContextualBackBoundary } from "@/components/navigation/ContextualBackBoundary";
 import OrdersPage from "@/screens/OrderHistory/OrderHistory";
 
 export default function OrdersRoutePage() {
   return (
-    <div className="pb-20 md:pb-0">
-      <ContextualBackBoundary destination="/orders">
-        <OrdersPage />
-      </ContextualBackBoundary>
-      <BottomNav />
-    </div>
+    <ContextualBackBoundary destination="/orders">
+      <OrdersPage />
+    </ContextualBackBoundary>
   );
 }
